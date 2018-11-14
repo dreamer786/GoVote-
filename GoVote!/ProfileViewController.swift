870492df9229784a6c33104bf2ab4bd4b074b42e
Kelly <kyn218@nyu.edu>
2018-11-14 18:58:09 -0500

    profile view controller

---
+++ GoVote!/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  GoVote!
+//
+//  Created by Kelly Ng on 11/14/18.
+//  Copyright © 2018 nyu.edu. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
