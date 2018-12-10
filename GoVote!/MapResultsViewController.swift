68a8ea667ea70a3cb62e864886f852ac675be34a
Kelly <kyn218@nyu.edu>
2018-12-09 20:45:54 -0500

    updates on map and getting info from map (not done yet)

---
+++ GoVote!/MapResultsViewController.swift
@@ -0,0 +1,60 @@
+//
+//  MapResultsViewController.swift
+//  GoVote!
+//
+//  Created by Kelly Ng on 12/9/18.
+//  Copyright © 2018 nyu.edu. All rights reserved.
+//  modified version of QueryResultsTableViewController
+//
+
+import UIKit
+import Firebase 
+
+class MapResultsViewController: UITableViewController{
+    
+    var state: String? = ""
+    var myData: DatabaseReference!
+    var results = [Dictionary<String, String>]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myData = Database.database().reference()
+        retrieveSenators()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func retrieveSenators(){
+        let _ = myData.observe(.childAdded, with: { snapshot in
+            let dict = snapshot.value as![String: String]
+            let myState: String? = dict["state"]
+            if myState == self.state{
+                self.results.append(dict)
+            }
+            self.tableView.reloadData()
+        }) {(error) in
+            print("Failed to get snapshot", error.localizedDescription)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.results.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let res:[String:String] = results[indexPath.row]
+        cell.textLabel?.text = "\(res["firstName"]!) \(res["lastName"]!)"
+        // Configure the cell...
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc: ProfileViewController = storyboard?.instantiateViewController(withIdentifier: "candidateView") as! ProfileViewController
+        vc.candidateInfo = results[indexPath.row]
+        show(vc, sender: nil)
+    }
+    
+}
--- GoVote!/MapViewController.swift
+++ GoVote!/MapViewController.swift
@@ -13,6 +13,7 @@ import MapKit
 class MapViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
+    var state:String = ""
     
     let locationManager = CLLocationManager()
     var location: CLLocation!
@@ -25,11 +26,54 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
         mapView.showsUserLocation = true
+        locationManager.requestLocation()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        /*if let lat = locations.last?.coordinate.latitude, let long = locations.last?.coordinate.longitude {
+        }*/
+        self.location = locations.last as CLLocation!
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
+    
+    func lookUpCurrentLocation(completionHandler: @escaping (CLPlacemark?) -> Void ) {
+
+        if let lastLocation = self.locationManager.location {
+            let geocoder = CLGeocoder()
+            
+            geocoder.reverseGeocodeLocation(lastLocation, completionHandler: { (placemarks, error) in
+                if error == nil {
+                    let myLocation = placemarks?[0]
+                    if myLocation?.administrativeArea != nil {
+                        self.state = (myLocation?.administrativeArea)!
+                    }
+                    completionHandler(myLocation)
+                }
+                else {
+                    completionHandler(nil)
+                }
+            })
+        }
+        else {
+            completionHandler(nil)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+  
+        let myDestination = segue.destination as! MapResultsViewController
+        
+        myDestination.state = state
+        
+        
+    }
+    
 
 }
-
